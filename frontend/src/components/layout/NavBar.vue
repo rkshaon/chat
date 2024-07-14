3d989794b260de6df4a872b91427270d0b02e218
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-07-15 01:27:00 +0600

    frontend: home page, NavBar layout

---
+++ frontend/src/components/layout/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav class="bg-gray-800 text-white p-10">
+        <ul class="flex space-x-4">
+            <li><a href="#" class="hover:underline">Home</a></li>
+            <li><a href="#" class="hover:underline">About</a></li>
+            <li><a href="#" class="hover:underline">Contact</a></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+    props: { }, 
+}
+</script>
---
+++ frontend/src/pages/HomeComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col h-screen">
+    <NavBar />
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/layout/NavBar.vue';
+
+export default {
+  name: 'HomeComponent',
+  props: {},
+  components: {
+    NavBar,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
