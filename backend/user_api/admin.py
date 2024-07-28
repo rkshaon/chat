726ef3b099b521a4a33aabf1106a364eacde62a9
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-07-28 16:54:43 +0600

    backend: user - admin

--- backend/user_api/admin.py
+++ backend/user_api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from user_api.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'name', 'is_superuser', 'is_active']
+    list_filter = ['is_superuser', 'is_staff', 'is_active',]
+    search_fields = ['username', 'email', 'name',]
