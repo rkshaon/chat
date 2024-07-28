5f4562ea7ac066c84df04786dd44fdcd5688f2da
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-07-28 16:51:20 +0600

    backend: user - model

--- backend/user_api/models.py
+++ backend/user_api/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        user = self.create_user(email, password, **extra_fields)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True
+    )
+    picture = models.ImageField(
+        upload_to='profile',
+        null=True,
+        blank=True
+    )
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
