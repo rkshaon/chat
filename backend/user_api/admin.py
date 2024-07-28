e5ee318aafd4559542bed60ed312ec055eeec1a1
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-07-28 17:22:07 +0600

    backend: user add from admin panel
    password was not hashed, so I updated admin.py and now on insert a new user or update an existing user - both time password is hashed

--- backend/user_api/admin.py
+++ backend/user_api/admin.py
@@ -8,3 +8,9 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ['username', 'email', 'name', 'is_superuser', 'is_active']
     list_filter = ['is_superuser', 'is_staff', 'is_active',]
     search_fields = ['username', 'email', 'name',]
+
+    def save_model(self, request, obj, form, change):
+        if "password" in form.changed_data:
+            obj.set_password(obj.password)
+
+        super().save_model(request, obj, form, change)
